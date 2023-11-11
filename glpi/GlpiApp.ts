56d7593e05ec470f51e4f6b26017f3d174cfd6ad
Diego RA <diego@qosit.com.br>
2023-11-10 21:16:40 -0300

    Change method executePostLivechatRoomClosed

--- glpi/GlpiApp.ts
+++ glpi/GlpiApp.ts
@@ -2,13 +2,24 @@ import {
     IAppAccessors,
     IConfigurationExtend,
     IEnvironmentRead,
+    IHttp,
     ILogger,
+    IModify,
+    IPersistence,
+    IRead,
 } from "@rocket.chat/apps-engine/definition/accessors";
 import { App } from "@rocket.chat/apps-engine/definition/App";
-import { IAppInfo } from "@rocket.chat/apps-engine/definition/metadata";
+import {
+    AppMethod,
+    IAppInfo,
+} from "@rocket.chat/apps-engine/definition/metadata";
 import { settings } from "./src/settings/settings";
+import {
+    ILivechatRoom,
+    IPostLivechatRoomClosed,
+} from "@rocket.chat/apps-engine/definition/livechat";
 
-export class GlpiApp extends App {
+export class GlpiApp extends App implements IPostLivechatRoomClosed {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
     }
@@ -20,5 +31,26 @@ export class GlpiApp extends App {
         await settings.forEach((setting) =>
             configuration.settings.provideSetting(setting)
         );
+    }
+
+    public async executePostLivechatRoomClosed(
+        room: ILivechatRoom,
+        read: IRead,
+        http: IHttp,
+        persis: IPersistence,
+        modify?: IModify | undefined
+    ): Promise<void> {
+        // Pegar dados da conversa
+        // const data = ProcessDataService.ProcessData('LivechatSession',read,persistence, room,undefined,this.getLogger());
+        const data = "dados";
+
+        if (!data) {
+            return;
+        }
+        // Atualizar o chamado
+        // const TicketID = await CloseChatService.CloseChat(http, read, this.getLogger(), data)]
+        const ticketID = "Número do Chamado";
+
+        return;
     }
 }
