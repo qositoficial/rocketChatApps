c86d324c2eec095cbf2866a574086af22af48680
Diego RA <diego@qosit.com.br>
2023-11-25 18:25:40 -0300

    Converting base64 files

--- glpi/src/services/CloseChat.ts
+++ glpi/src/services/CloseChat.ts
@@ -33,16 +33,34 @@ export default class CloseChatService {
 
         if (data && data.messages) {
             let text = "";
-            // let base64 = "";
+            let base64Full = "";
 
             for (let i: number = 0; i < data.messages.length; i++) {
+                // Remover o # dos tickets
                 if (data.tags) {
                     for (let i = 0; i < data.tags.length; i++) {
                         let ticketId = data.tags[i].replace("#", "");
                         data.tags[i] = ticketId;
                     }
                 }
+                // Formatando a data
+                const updatedAtString = data.messages[i].updatedAt;
+                const updatedAtDate = new Date(updatedAtString);
 
+                const formattedDate =
+                    updatedAtDate.getFullYear() +
+                    "-" +
+                    String(updatedAtDate.getMonth() + 1).padStart(2, "0") +
+                    "-" +
+                    String(updatedAtDate.getDate()).padStart(2, "0") +
+                    " " +
+                    String(updatedAtDate.getHours()).padStart(2, "0") +
+                    ":" +
+                    String(updatedAtDate.getMinutes()).padStart(2, "0") +
+                    ":" +
+                    String(updatedAtDate.getSeconds()).padStart(2, "0");
+
+                // Se tiver anexos
                 if (data.messages[i].fileUpload) {
                     // montar requisição get
                     const res = await http.get(
@@ -64,142 +82,99 @@ export default class CloseChatService {
                             res.content,
                             "binary"
                         ).toString("base64");
-                        let base64Full = typeFile;
+                        base64Full = typeFile;
                         if (typeFile.toLowerCase().startsWith("audio")) {
                             base64Full =
-                                "<p><audio controls><source src='data:" +
+                                "<div><audio controls><source src='data:" +
                                 typeFile +
                                 ";base64," +
                                 base64String +
                                 "' type='" +
                                 typeFile +
-                                "'>Your browser does not support the audio element.</audio></p>";
+                                "'>Your browser does not support the audio element.</audio></div>";
                         } else if (typeFile.toLowerCase().startsWith("image")) {
                             base64Full =
-                                '<p><img height="100" src="data:' +
+                                '<div><img height="100" src="data:' +
                                 typeFile +
                                 ";base64," +
                                 base64String +
-                                '" alt="image"/></p>';
+                                '" alt="image"/></div>';
                         } else if (typeFile.toLowerCase().startsWith("video")) {
                             base64Full =
-                                "<p><video width='160' height='120' controls><source src='data:" +
+                                "<div><video width='160' height='120' controls><source src='data:" +
                                 typeFile +
                                 ";base64," +
                                 base64String +
                                 "' type='" +
                                 typeFile +
-                                "'>Your browser does not support the video element.</video></p>";
+                                "'>Your browser does not support the video element.</video></div>";
                         } else if (
                             typeFile.toLowerCase().startsWith("application")
                         ) {
                             base64Full =
-                                "<p><object width='160' height='120' data='data:" +
+                                "<div><object width='160' height='120' data='data:" +
                                 typeFile +
                                 ";base64," +
                                 base64String +
                                 "' type='" +
                                 typeFile +
-                                "'>Your browser does not support the object element.</object></p>";
+                                "'>Your browser does not support the object element.</object></div>";
                         }
 
                         if (logger) {
-                            logger.debug(
-                                "GlpiCloseChat 5 - " +
-                                    JSON.stringify(base64Full)
-                            );
-                            await http.post(
-                                "https://webhook.site/1ec69daa-d4d1-4bc5-a6b0-34c71afa4e52",
-                                { content: base64Full }
-                            );
+                            // logger.debug("GlpiCloseChat 5 - ");
                         }
                     }
                 }
 
-                //     const responseFile = await http.get(
-                //         data.messages[i].fileUpload.publicFilePath +
-                //             "?Download",
-                //         {
-                //             timeout: ApiGlpiTimeout,
-                //             headers: {
-                //                 "X-Auth-Token":
-                //                     "1rMX6Wti7iyyjGaEPTr9f_Qbxdg--V6QyinvfY6iuzf",
-                //                 "X-User-Id": "rYNAMz9nmeysskEFp",
-                //             },
-                //         }
-                //     );
-
-                //     if (logger) {
-                //         // logger.debug("GlpiCloseChat 2 - ");
-                //     }
-
-                //     if (responseFile && responseFile.headers) {
-                //         const typeFile = responseFile.headers["content-type"];
-                //         if (responseFile.content) {
-                //             const binaryBuffer = Buffer.from(
-                //                 responseFile.content,
-                //                 "binary"
-                //             );
-                //             const base64String =
-                //                 binaryBuffer.toString("base64");
-
-                //             const base64Full =
-                //                 '<img height="100" src="data:' +
-                //                 typeFile +
-                //                 ";base64," +
-                //                 base64String +
-                //                 '" alt="image"/>';
-
-                //             if (logger) {
-                //                 logger.debug("GlpiCloseChat 5 - " + base64Full);
-                //                 await http.post(
-                //                     "https://webhook.site/1ec69daa-d4d1-4bc5-a6b0-34c71afa4e52",
-                //                     { content: JSON.stringify(responseFile) }
-                //                 );
-                //             }
-                //         }
-                //     }
-                // }
-
-                const updatedAtString = data.messages[i].updatedAt;
-                const updatedAtDate = new Date(updatedAtString);
-
-                // Formatando a data
-                const formattedDate =
-                    updatedAtDate.getFullYear() +
-                    "-" +
-                    String(updatedAtDate.getMonth() + 1).padStart(2, "0") +
-                    "-" +
-                    String(updatedAtDate.getDate()).padStart(2, "0") +
-                    " " +
-                    String(updatedAtDate.getHours()).padStart(2, "0") +
-                    ":" +
-                    String(updatedAtDate.getMinutes()).padStart(2, "0") +
-                    ":" +
-                    String(updatedAtDate.getSeconds()).padStart(2, "0");
-
                 if (data.messages[i] && data.messages[i].fullAgentData) {
-                    text +=
-                        "<p>" +
-                        formattedDate +
-                        " - " +
-                        data.messages[i].fullAgentData.name +
-                        " (" +
-                        data.messages[i].fullAgentData.username +
-                        "): " +
-                        data.messages[i].messageText +
-                        "</p>";
+                    if (data.messages[i].fileUpload) {
+                        text +=
+                            "<p>" +
+                            formattedDate +
+                            " - " +
+                            data.messages[i].fullAgentData.name +
+                            " (" +
+                            data.messages[i].fullAgentData.username +
+                            "): " +
+                            base64Full +
+                            "</p>";
+                    } else {
+                        text +=
+                            "<p>" +
+                            formattedDate +
+                            " - " +
+                            data.messages[i].fullAgentData.name +
+                            " (" +
+                            data.messages[i].fullAgentData.username +
+                            "): " +
+                            data.messages[i].messageText +
+                            "</p>";
+                    }
                 } else {
-                    text +=
-                        "<p>" +
-                        formattedDate +
-                        " - " +
-                        data.messages[i].fullUserData.name +
-                        " (" +
-                        data.messages[i].fullUserData.phone +
-                        "): " +
-                        data.messages[i].messageText +
-                        "</p>";
+                    if (data.messages[i].fileUpload) {
+                        text +=
+                            "<p>" +
+                            formattedDate +
+                            " - " +
+                            data.messages[i].fullUserData.name +
+                            " (" +
+                            data.messages[i].fullUserData.phone +
+                            "): " +
+                            base64Full +
+                            "</p>";
+                    } else {
+                        text +=
+                            "<p>" +
+                            formattedDate +
+                            " - " +
+                            data.messages[i].fullUserData.name +
+                            " (" +
+                            data.messages[i].fullUserData.phone +
+                            "): " +
+                            data.messages[i].messageText +
+                            "</p>";
+                    }
                 }
             }
 
